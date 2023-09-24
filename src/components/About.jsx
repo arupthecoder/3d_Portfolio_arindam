ba078c5b47f56434c2d03e4f55dc5f00b5197a76
arupthecoder <119742142+arupthecoder@users.noreply.github.com>
2023-09-24 09:39:11 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon}) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn("right","spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} 
+          className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}
+        >Introduction</p>
+        <h2 className={styles.sectionHeadText}
+        >Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[14px] max-w-3xl leading-[25px]'
+      >
+      I am Undergraduate student seeking a Job, and my objective is
+      to work in a healthy environment where I can utilize my
+      potentials to work for an organization as well as to develop
+      my skills and build a successful career.I possess basic skills in JavaScript, and frameworks like React, Node.js.I analyse complex query and create efficient, scalable, and user-friendly solutions that solve real-world problems.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, 'about') ;
